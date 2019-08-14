3778c119ed9ef97b8e12ba0c8450f98d7e197e03
root <root@9dd5d0d9-c142-4e65-927f-6e824c869eee.apple.com>
2019-08-14 23:41:34 +0000

    download the model

---
+++ research/object_detection/object_detection_downloadmodel.py
@@ -0,0 +1,25 @@
+import os
+import six.moves.urllib as urllib
+import tarfile
+
+# What model to download.
+MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_oid_v4_2018_12_12'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'oid_v4_label_map.pbtxt')
+
+NUM_CLASSES = 601
+
+
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+  file_name = os.path.basename(file.name)
+  if 'frozen_inference_graph.pb' in file_name:
+    tar_file.extract(file, os.getcwd())
